da3b78ebb44cb20d586106ebae1adb7800361948
jeay <wrjie@msn.cn>
2018-04-10 17:28:56 +0800

    列表

---
+++ src/components/pages/Index.vue
@@ -0,0 +1,39 @@
+/*
+ * @Author: jeay
+ * @Date: 2018-04-10 13:47:15
+ * @Last Modified by: jeay
+ * @Last Modified time: 2018-04-10 16:59:15
+ */
+<template>
+  <div class="list">
+    <el-row>
+      <el-col :span="2">id</el-col>
+      <el-col :span="4">姓名</el-col>
+      <el-col :span="4">电话</el-col>
+      <el-col :span="5">邮箱</el-col>
+      <el-col :span="4">院系</el-col>
+      <el-col :span="5">班级</el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="sass" scoped>
+.list
+  padding: 5px
+  .el-col
+    height: 28px
+    line-height: 28px
+    border-bottom: 1px solid #eee
+</style>
---
+++ src/components/pages/User.vue
@@ -0,0 +1,17 @@
+/*
+ * @Author: jeay
+ * @Date: 2018-04-10 15:47:34
+ * @Last Modified by: jeay
+ * @Last Modified time: 2018-04-10 17:05:38
+ */
+<template>
+  <div class="list">
+    <div class="adduser">
+      <el-button type="primary">添加用户</el-button>
+    </div>
+  </div>
+</template>
+<style lang="sass" scoped>
+.adduser
+  text-align: left
+</style>
