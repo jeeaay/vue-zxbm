7b8dd10ce1ea4d9fb1e96a8f7dd9d5e8eb7bc072
jeay <wrjie@msn.cn>
2018-04-10 17:27:34 +0800

    路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/pages/Login'
+import Home from '@/components/common/Home'
+import Index from '@/components/pages/Index'
+import User from '@/components/pages/User'
 
 Vue.use(Router)
 
@@ -11,7 +14,8 @@ let router = new Router({
       name: 'Login',
       meta: { title: '请登录！' },
       component: Login
-    }/* ,
+    },
+    /*
     {
       path: '/SignIn',
       name: 'SignIn',
@@ -25,7 +29,7 @@ let router = new Router({
         requireAuth: true
       },
       component: [Login, SignIn]
-    },
+    }, */
     {
       path: '/admin',
       component: Home,
@@ -34,17 +38,13 @@ let router = new Router({
       },
       children: [
         { path: '/', component: Index, name: '首页', meta: { title: '案例采集后台管理' } },
-        { path: 'users', component: Users, name: '用户管理', meta: { title: '用户管理' } },
-        { path: 'reviewUser', component: reviewUser, name: '审核用户', meta: { title: '审核用户' } },
-        { path: 'category', component: Cates, name: '分类管理', meta: { title: '分类管理' } },
-        { path: 'cases', component: Cases, name: '案例管理', meta: { title: '案例管理' } },
-        { path: 'xiugai', component: Xiugai, name: '修改密码', meta: { title: '修改密码' } }
+        { path: 'user', component: User, name: '用户管理', meta: { title: '用户管理' } }
       ]
-    } */
+    }
   ]
 })
 
-router.beforeEach((to, from, next) => {
+/* router.beforeEach((to, from, next) => {
   let token = localStorage.getItem('access_token')
   let nowTime = new Date()
   if (to.matched.some(r => r.meta.requireAuth)) {
@@ -60,11 +60,10 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
-  /* 路由发生变化修改页面title */
   if (to.meta.title) {
     document.title = to.meta.title
   }
   next()
-})
+}) */
 
 export default router
