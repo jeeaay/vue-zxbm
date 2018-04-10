4d52e88712819abec39a95ebc800a60e9d27b65a
jeay <wrjie@msn.cn>
2018-04-10 17:27:50 +0800

    引入

--- src/main.js
+++ src/main.js
@@ -3,19 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { Form, FormItem, Input, Button } from 'element-ui'
+import { Form, FormItem, Input, Button, Menu, MenuItem, Row, Col } from 'element-ui'
 
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Row)
+Vue.use(Col)
 
 /* eslint-disable no-new */
 
 new Vue({
   el: '#app',
   router,
-  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
