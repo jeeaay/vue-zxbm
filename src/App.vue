f6caeb88df4cf5e68d401e788a608a93f0789c52
jeay <wrjie@msn.cn>
2018-04-10 17:27:13 +0800

    公共样式

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,18 @@ export default {
   name: 'App'
 }
 </script>
-<style lang="sass" scoped>
+<style lang="sass">
+body
+  padding: 0
+  margin: 0
+  box-sizing: border-box
+div
+  box-sizing: border-box
+a
+  text-decoration: none
 #app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
   color: #2c3e50
-  margin-top: 60px
 </style>
