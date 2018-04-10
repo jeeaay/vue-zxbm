081b4343b84c9c8a2ab79f921b7438919e79e4e4
jeay <wrjie@msn.cn>
2018-04-10 14:39:37 +0800

    登陆

---
+++ src/components/pages/Login.vue
@@ -0,0 +1,62 @@
+/*
+ * @Author: jeay
+ * @Date: 2018-04-10 13:47:15
+ * @Last Modified by: jeay
+ * @Last Modified time: 2018-04-10 14:37:36
+ */
+<template>
+  <div>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="name" required>
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="passwd" required>
+        <el-input type="password" v-model="ruleForm.passwd"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ruleForm: {
+        name: '',
+        passwd: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
+        ],
+        passwd: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          console.log(this.ruleForm)
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,70 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/pages/Login'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Login',
+      meta: { title: '请登录！' },
+      component: Login
+    }/* ,
+    {
+      path: '/SignIn',
+      name: 'SignIn',
+      meta: {title: '注册用户'},
+      component: SignIn
+    },
+    {
+      path: '/repository',
+      name: 'repository',
+      meta: {
+        requireAuth: true
+      },
+      component: [Login, SignIn]
+    },
+    {
+      path: '/admin',
+      component: Home,
+      meta: {
+        requireAuth: true
+      },
+      children: [
+        { path: '/', component: Index, name: '首页', meta: { title: '案例采集后台管理' } },
+        { path: 'users', component: Users, name: '用户管理', meta: { title: '用户管理' } },
+        { path: 'reviewUser', component: reviewUser, name: '审核用户', meta: { title: '审核用户' } },
+        { path: 'category', component: Cates, name: '分类管理', meta: { title: '分类管理' } },
+        { path: 'cases', component: Cases, name: '案例管理', meta: { title: '案例管理' } },
+        { path: 'xiugai', component: Xiugai, name: '修改密码', meta: { title: '修改密码' } }
+      ]
+    } */
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem('access_token')
+  let nowTime = new Date()
+  if (to.matched.some(r => r.meta.requireAuth)) {
+    if (token && (nowTime.getTime() < localStorage.getItem('expires_time') * 1000)) {
+      next()
+    } else {
+      localStorage.clear()
+      next({
+        path: '/',
+        query: { redirect: to.fullPath }
+      })
+    }
+  } else {
+    next()
+  }
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
+
+export default router
