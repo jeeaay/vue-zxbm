aefd8a26c987feee25bea84bccd5269d76669dd1
jeay <wrjie@msn.cn>
2018-04-10 17:28:07 +0800

    登陆

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -2,10 +2,11 @@
  * @Author: jeay
  * @Date: 2018-04-10 13:47:15
  * @Last Modified by: jeay
- * @Last Modified time: 2018-04-10 14:37:36
+ * @Last Modified time: 2018-04-10 16:05:49
  */
 <template>
-  <div>
+  <div class="login">
+    <h1>后台登陆</h1>
     <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
       <el-form-item label="用户名" prop="name" required>
         <el-input v-model="ruleForm.name"></el-input>
@@ -13,10 +14,8 @@
       <el-form-item label="密码" prop="passwd" required>
         <el-input type="password" v-model="ruleForm.passwd"></el-input>
       </el-form-item>
-      <el-form-item>
         <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
-      </el-form-item>
     </el-form>
   </div>
 </template>
@@ -60,3 +59,16 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.login
+  border: 1px solid #eaeaea
+  box-shadow: 0 0 25px #cac6c6
+  width: 250px
+  margin: 60px auto
+  padding: 5px 20px 20px 5px
+  @media (min-width: 330px)
+    width: 280px
+    padding: 10px 40px 40px 10px
+  @media (min-width: 760px)
+    width: 500px
+</style>
