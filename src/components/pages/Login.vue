90307805b771c37e9080d9e227aeb62960e28e6d
jeay <wrjie@msn.cn>
2018-04-11 17:58:10 +0800

    登陆

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -2,7 +2,7 @@
  * @Author: jeay
  * @Date: 2018-04-10 13:47:15
  * @Last Modified by: jeay
- * @Last Modified time: 2018-04-10 16:05:49
+ * @Last Modified time: 2018-04-11 17:26:47
  */
 <template>
   <div class="login">
@@ -14,13 +14,16 @@
       <el-form-item label="密码" prop="passwd" required>
         <el-input type="password" v-model="ruleForm.passwd"></el-input>
       </el-form-item>
-        <el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+        <el-button type="primary" @click="subLogin('ruleForm')">登陆</el-button>
         <el-button @click="resetForm('ruleForm')">重置</el-button>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import qs from 'qs'
+import { Message } from 'element-ui'
 export default {
   data () {
     return {
@@ -30,7 +33,7 @@ export default {
       },
       rules: {
         name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { required: true, message: '请输入用户名', trigger: 'blur' },
           { min: 3, max: 16, message: '长度在 3 到 16 个字符', trigger: 'blur' }
         ],
         passwd: [
@@ -40,13 +43,34 @@ export default {
     }
   },
   mounted () {
+    let nowTime = new Date()
+    let token = localStorage.getItem('access_token')
+    if (token && (nowTime.getTime() < localStorage.getItem('expires_time') * 1000)) {
+      Message.success({message: '登陆成功!'})
+      this.$router.push({path: (sessionStorage.getItem('activeIndex') ? sessionStorage.getItem('activeIndex') : '/admin/')})
+    }
   },
   methods: {
-    submitForm (formName) {
-      this.$refs[formName].validate((valid) => {
+    subLogin (formName) {
+      const self = this
+      this.$refs[formName].validate(async (valid) => {
         if (valid) {
-          console.log(this.ruleForm)
-          alert('submit!')
+          let msg = await axios.post('/login', qs.stringify({username: this.ruleForm.name, password: this.ruleForm.passwd}))
+          if (msg.data.error) {
+            console.log('账户或密码错误')
+          } else {
+            Message.success({message: '登陆成功'})
+            localStorage.setItem('access_token', msg.data.access_token)
+            localStorage.setItem('expires_time', msg.data.time)
+            localStorage.setItem('userID', msg.data.user_id)
+            localStorage.setItem('real_name', msg.data.real_name)
+            setTimeout(function () {
+              self.$router.push({
+                path: (sessionStorage.getItem('activeIndex') ? sessionStorage.getItem('activeIndex') : '/admin/')
+              })
+            }, 300)
+          }
+          // alert('submit!')
         } else {
           console.log('error submit!!')
           return false
