db880090c2248bb774ed7c9befdccceb919fa7db
jeay <wrjie@msn.cn>
2018-04-12 09:14:31 +0800

    404页面

---
+++ src/components/pages/Notfound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>404 Error</h1>
+      <el-button type="primary" @click="goHistory">返回</el-button>
+    </div>
+</template>
+<script>
+export default {
+  methods: {
+    goHistory () {
+      this.$router.push({path: (sessionStorage.getItem('activeIndex') ? sessionStorage.getItem('activeIndex') : '/')})
+    }
+  }
+}
+</script>
